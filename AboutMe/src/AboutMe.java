e943435ef3a1dd476f44165d5ad18a03c8a59d6a
GioByte10 <gvanni.bernal10@gmail.com>
2019-08-25 01:35:44 -0700

    Java

---
+++ AboutMe/src/AboutMe.java
@@ -0,0 +1,13 @@
+
+public class AboutMe{
+  public static void main(String[] args){
+  
+     System.out.println("My Name: Giovanni Bernal");
+     System.out.println("My Birthday: 23/09/2002");
+     System.out.println("My Hobbies: Cycling, Fishing, Chess");
+     System.out.println("My Favorite Movie: Project Almanac");
+     System.out.print("My Favorite Subject: Computer Science & Engineering");
+     
+  
+  }
+}
---
+++ Test/src/Program.java
@@ -0,0 +1,11 @@
+public class Lesson_1_Activity_Three {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("********");
+		System.out.println("* java *");
+		System.out.print("********");
+		
+	}
+
+}
