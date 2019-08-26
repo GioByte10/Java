452bb6524533ec7e0884e32606994c6dca279261
GioByte10 <gvanni.bernal10@gmail.com>
2019-08-26 13:05:36 -0700

    Java

---
+++ Math/src/Math.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Math {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		double x, y;
+		
+		System.out.println("Enter 2 decimal numbers");
+		
+		x = scan.nextDouble();
+		y = scan.nextDouble();
+		
+		System.out.print(x + " " + y);
+		
+		
+	}
+
+}
--- Test/src/Program.java
+++ Test/src/Program.java
@@ -1,11 +1,37 @@
-public class Lesson_1_Activity_Three {
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void Function(int x) {
+		System.out.println("The square of \n" + x + " is " + x*x);
+	}
+	
+	public static void askName() {
+		
+		Scanner scan = new Scanner(System.in);
+		String name, adjective;
+		
+		System.out.println("Hi there. What is your name?");
+		name = scan.nextLine();
+		System.out.println("What adjective describes you?");
+        adjective = scan.nextLine();
+		System.out.print("My name is " + name + ". I am " + adjective);
+		
+	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println("********");
-		System.out.println("* java *");
-		System.out.print("********");
 		
+		Scanner scanw = new Scanner(System.in);		
+		System.out.println("Enter a number");
+		int num = scanw.nextInt();
+		
+		Function(num);
+		
+		askName();
+
+   
 	}
+
 
 }
