9558a34f9fb931627db5fc6167beddd63bac56cf
GioByte10 <gvanni.bernal10@gmail.com>
2019-09-19 13:26:28 -0700

    Java

--- Math/src/Math.java
+++ Math/src/Math.java
@@ -1,33 +1,23 @@
-import java.util.Scanner;
-public class Math {
-	
-  public static void main(String[] params) {
-	  
-	  Scanner scan = new Scanner(System.in);
-	  
-	  int in = scan.nextInt();
-	  
-	  if(in < 0 || in > 100) {
-		  
-		  System.out.println("error");
-		  
-	  }else {
-	  
-    for  (int i = in; i <= 100; i++) {
-    	
-    	System.out.print(i + " ");
-    	
-   
-        if ((i + 1 - in) % 20 == 0) {
-        	
-        	System.out.println("");
-        	
-        }
-      
-      
-    }
-	  }
-    
-  }
-}
+import javax.swing.*;  
+public class Math {  
 
+    
+ 
+public static void main(String[] args) {  
+	String test;
+	//JOptionPane.showMessageDialog(null, "Alert", "Alert", JOptionPane.WARNING_MESSAGE);
+	//JOptionPane.showInternalMessageDialog(frame, "information",
+            //"information", JOptionPane.INFORMATION_MESSAGE);
+	
+	JOptionPane.showConfirmDialog(null,
+            "choose one", "choose one", JOptionPane.YES_NO_OPTION);
+	
+	
+	/*ERROR_MESSAGE
+	INFORMATION_MESSAGE
+	WARNING_MESSAGE
+	QUESTION_MESSAGE
+	PLAIN_MESSAGE*/
+    
+}  
+}  
