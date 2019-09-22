9c687491fb7828d6080080b2afd2b766c33caa09
GioByte10 <gvanni.bernal10@gmail.com>
2019-09-22 01:58:23 -0700

    Java

---
+++ 1.- Assignments/Guessing Game/src/GuessingGame.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+import java.math;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		
+		String rangeOrNot = "", keepPlaying = ""; 
+		int MaxValue = 0, NumToGuess = 0, count = 0, LastNumber = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Guessing Game!");
+		System.out.println("To win, you have to either guess the number from 1 to whatever or a range you can select");
+		
+		
+		
+		do {
+			
+			while(!(rangeOrNot.toLowerCase().contentEquals("yes") || rangeOrNot.toLowerCase().contentEquals("no"))) {
+				
+			System.out.println("Do you want to chose a range? (Yes/No)");
+			rangeOrNot = scan.nextLine();
+			
+			System.out.println(rangeOrNot.toLowerCase());
+			
+			if(rangeOrNot.toLowerCase().equals("yes")) {
+				
+				System.out.println("Enter the Max Value of the range (No more than 2147483647)");	
+				MaxValue = scan.nextInt();
+				NumToGuess = (int) (Math.random() * MaxValue + 1);
+				
+                
+				
+				
+				
+			}else if (rangeOrNot.toLowerCase().equals("no")) {
+				
+				NumToGuess = (int) (Math.random() * 2147483647 + 1);	
+				
+				
+			}
+				
+			}
+			
+			System.out.println(NumToGuess);
+			System.out.println("Guess the Number!");
+			
+			while(LastNumber != NumToGuess) {
+				
+				LastNumber = scan.nextInt();
+				
+			   if(LastNumber > NumToGuess) 
+				   System.out.println("Enter a smaller number");
+			   
+			   else if(LastNumber < NumToGuess) 
+				   System.out.println("Enter a bigger number");
+			   
+			   count++;
+			   
+			}
+			
+			scan.nextLine();
+			rangeOrNot = "";
+			LastNumber = 0;
+			
+			System.out.println("Congratulations! You Guessed the number!, It only took you " + count + " attemps");
+			System.out.println("Do you want to keep playing? (yes/no)");
+			
+			count = 0;			
+			keepPlaying = scan.nextLine();
+			
+						
+		}while(keepPlaying.toLowerCase().contentEquals("yes"));
+		
+		
+		scan.close();
+		
+
+	}
+
+}
--- Average/src/Average.java
+++ Average/src/Average.java
@@ -4,38 +4,9 @@ class Average {
   public static void main(String[] args) {
     
 	  Scanner scan = new Scanner(System.in);
+	  int x = 2147483647;
 	  
-	  int test[] = new int[5];
-	  
-	  test[0] = 0;
-	  test[1] = 1;
-	  test[2] = 2;
-	  test[3] = 3;
-	  test[4] = 4;
-	  
-      for (int k = 0; k < test.length; k++) {
-		  
-		  System.out.print(test[k] + " ");
-		  
-	  }
-	  
-	  
-	  int ins = scan.nextInt();
-	  int ins2 = ins;
-	  
-	  for (int i = ins + 1; i < test.length; i++) {
-		  
-		  test[i - 1] = test[i];
-		  
-	  }
-	  
-	  
-	  
-	  for (int j = 0; j < test.length; j++) {
-		  
-		  System.out.print(test[j] + " ");
-		  
-	  }
+	  System.out.println(x);
 	  
 	  
 	  scan.close();
