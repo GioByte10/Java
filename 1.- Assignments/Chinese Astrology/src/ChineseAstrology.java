326dea95606fc25fe6430a14245d7b442b9cc317
GioByte10 <gvanni.bernal10@gmail.com>
2019-11-09 13:28:15 -0800

    Java

--- 1.- Assignments/Chinese Astrology/src/ChineseAstrology.java
+++ 1.- Assignments/Chinese Astrology/src/ChineseAstrology.java
@@ -2,94 +2,15 @@ import java.util.Scanner;
 
 public class ChineseAstrology {
 	
-public static void Monkey() {
 		
-		System.out.println("Your Chinese zodiac sign is a Monkey");
-		System.out.println("The Monkey is the ninth of 12 animals in the recurring 12-year Chinese zodiac cycle.");
-		System.out.println("People born in a year of the Monkey have magnetic personalities and are witty and intelligent. Personality traits like mischievousness, curiosity, and cleverness make them very naughty.");
-		
-	}
 	
-    public static void Rooster() {
-		
-		System.out.println("Your Chinese zodiac sign is a Rooster");
-		System.out.println("The Rooster is 10th among the 12 animals in the Chinese zodiac cycle. Every 12 years is a Rooster year, beginning at Chinese New Year.");
-		System.out.println("People born in a year of the Rooster are very observant. Hardworking, resourceful, courageous, and talented, Roosters are very confident in themselves.");
-		
-	}
-
-    public static void Dog() {
-	
-	System.out.println("Your Chinese zodiac sign is a Dog");
-	System.out.println("The Dog occupies the 11th position in the Chinese zodiac after the Rooster and before the Pig.");
-	System.out.println("Dogs are loyal and honest, amiable and kind, cautious and prudent. Due to having a strong sense of loyalty and sincerity, Dogs will do everything for the person who they think is most important.");
-	
-    }
     
-    public static void Pig() {
-    	
-	System.out.println("Your Chinese zodiac sign is a Pig");
-	System.out.println("The Pig is the 12th of 12 animals in the recurring 12-year Chinese zodiac cycle.");
-	System.out.println("Pigs are diligent, compassionate, and generous. They have great concentration: once they set a goal, they will devote all their energy to achieving it.");
-	
-    }
     
-    public static void Rat() {
-    	
-	System.out.println("Your Chinese zodiac sign is a Rat");
-	System.out.println("The Year of the Rat is the first zodiac sign in the Chinese zodiac cycle.");
-	System.out.println("With strong intuition and quick responses, Rats always easily adapt themselves to a new environment.");
-	
-    }
     
-    public static void Ox() {
-    	
-	System.out.println("Your Chinese zodiac sign is an Ox");
-	System.out.println("The Ox occupies the second position in the Chinese Zodiac.");
-	System.out.println("Oxes are known for diligence, dependability, strength and determination. Having an honest nature, Oxes are strongly patriotic, have ideals and ambitions for life, and attach importance to family and work. ");
-	
-    }
+   
+ 
     
-    public static void Tiger() {
-    	
-	System.out.println("Your Chinese zodiac sign is a Tiger");
-	System.out.println("The Tiger ranks third among the animals in the Chinese zodiac.");
-	System.out.println("People born in a year of the Tiger are brave, competitive, unpredictable, and confident. They are very charming and well-liked by others. But sometimes they are likely to be impetuous, irritable, and overindulged.");
-	
-    }
-    
-    public static void Rabbit() {
-    	
-    	System.out.println("Your Chinese zodiac sign is a Rabbit");
-    	System.out.println("The Year of the Rabbit is fourth in the Chinese zodiac cycle.");
-    	System.out.println("Rabbits tend to be gentle, quiet, elegant, and alert as well as quick, skillful, kind, patient, and very responsible. However, they might be superficial, stubborn, melancholy, and overly-discreet. ");
-    	
-        }
-    
-    public static void Dragon() {
-    	
-    	System.out.println("Your Chinese zodiac sign is a Dragon");
-    	System.out.println("The Dragon is the fifth of the 12 Chinese zodiac animals.");
-    	System.out.println("Among the Chinese zodiac animals, the dragon is the sole imaginary animal. The Chinese dragon is the most vital and powerful beast in the Chinese zodiac, although Dragons have an infamous reputation for being a hothead and possessing a sharp tongue. ");
-    	
-        }
-    
-    public static void Snake() {
-    	
-    	System.out.println("Your Chinese zodiac sign is a Snake");
-    	System.out.println("The Snake occupies the sixth position in the Chinese zodiac.");
-    	System.out.println("In Chinese culture, the Snake is the most enigmatic animal among the 12 zodiac animals. People born in a year of the Snake are supposed to be the most intuitive.");
-    	
-        }
-    
-    public static void Horse() {
-    	
-    	System.out.println("Your Chinese zodiac sign is a Horse");
-    	System.out.println("The Horse occupies the seventh position in the Chinese zodiac, after the Snake and before the Goat.");
-    	System.out.println("People born in a year of the Horse are extremely animated, active and energetic. Horses love to be in a crowd, and they can usually be seen on such occasions as concerts, theater performances, meetings, sporting events, and parties.");
-    	
-        }
-    
+ 
     public static void Goat() {
     	
     	System.out.println("Your Chinese zodiac sign is a Goat");
@@ -97,62 +18,113 @@ public static void Monkey() {
     	System.out.println("People born in a year of the Goat are generally believed to be gentle, mild-mannered, shy, stable, sympathetic, amicable, and brimming with a strong sense of kindheartedness and justice.");
     	
         }
+    
+	public static void display(String animal, String message) {
+		
+		System.out.println("Your Chinese zodiac sign is a " + animal);
+		System.out.println(message);
+		
+	}
 
-
-
-
-	
-	public static void main(String []args) {
+       public static void main(String []args) {
 		
 		Scanner scan = new Scanner(System.in);
+		String animalS = "", message = "";
 		
 		System.out.println("What is your year or birth?");
 		int year = scan.nextInt();
 		
 		int animal = year % 12;
 		
-		if(animal == 0)
-			Monkey();
+		switch(animal) {
 		
-		else if (animal == 1)
-			Rooster();
-		
-		else if (animal == 2)
-			Dog();
-		
-		else if (animal == 3)
-			Pig();
-		
-		else if (animal == 4)
-			Rat();
-		
-		else if (animal == 5)
-			Ox();
-		
-		else if (animal == 6)
-			Tiger();
-		
-		else if (animal == 7)
-			Rabbit();
-		
-		else if (animal == 8)
-			Dragon();
-		
-		else if (animal == 9)
-			Snake();
-		
-		else if (animal == 10)
-			Horse();
-		
-		else if (animal == 11)
-			Goat();
+		case 0:
+			animalS = "Monkey";
+			message = "The Monkey is the ninth of 12 animals in the recurring 12-year Chinese zodiac cycle.\r\n" + 
+					"People born in a year of the Monkey have magnetic personalities and are witty and intelligent. Personality traits like mischievousness, curiosity, and cleverness make them very naughty.";
+		    break; 
+		    
+		case 1:
+			animalS = "Rooster";
+			message = "The Rooster is 10th among the 12 animals in the Chinese zodiac cycle. Every 12 years is a Rooster year, beginning at Chinese New Year.\r\n" + 
+					"People born in a year of the Rooster are very observant. Hardworking, resourceful, courageous, and talented, Roosters are very confident in themselves.";
+		    break;
+		    
+		case 2:
+			animalS = "Pig";
+			message = "The Dog occupies the 11th position in the Chinese zodiac after the Rooster and before the Pig.\r\n" +
+					"Dogs are loyal and honest, amiable and kind, cautious and prudent. Due to having a strong sense of loyalty and sincerity, Dogs will do everything for the person who they think is most important.";
+		    break;
+		    
+		case 3:
+			animalS = "Pig";
+			message = "The Pig is the 12th of 12 animals in the recurring 12-year Chinese zodiac cycle.\r\n" + 
+					"Pigs are diligent, compassionate, and generous. They have great concentration: once they set a goal, they will devote all their energy to achieving it.";			
+			break;
 			
+		case 4:
+			animalS = "Rat";
+			message = "The Year of the Rat is the first zodiac sign in the Chinese zodiac cycle.\r\n" + 
+					"With strong intuition and quick responses, Rats always easily adapt themselves to a new environment.";		
+			break;
+			
+		case 5:
+			animalS = "Ox";
+			message = "The Ox occupies the second position in the Chinese Zodiac.\r\n" + 
+					"Oxes are known for diligence, dependability, strength and determination. Having an honest nature, Oxes are strongly patriotic, have ideals and ambitions for life, and attach importance to family and work.";		
+			break;
+			
+		case 6:
+			animalS = "Tiger";
+			message = "The Tiger ranks third among the animals in the Chinese zodiac.\r\n" + 
+					"People born in a year of the Tiger are brave, competitive, unpredictable, and confident. They are very charming and well-liked by others. But sometimes they are likely to be impetuous, irritable, and overindulged.";		
+			break;
+			
+		case 7:
+			animalS = "Rabbit";
+			message = "The Year of the Rabbit is fourth in the Chinese zodiac cycle.\r\n" + 
+					"Rabbits tend to be gentle, quiet, elegant, and alert as well as quick, skillful, kind, patient, and very responsible. However, they might be superficial, stubborn, melancholy, and overly-discreet.";		
+			break;
+			
+		case 8:
+			animalS = "Dragon";
+			message = "The Dragon is the fifth of the 12 Chinese zodiac animals.\r\n" + 
+					"Among the Chinese zodiac animals, the dragon is the sole imaginary animal. The Chinese dragon is the most vital and powerful beast in the Chinese zodiac, although Dragons have an infamous reputation for being a hothead and possessing a sharp tongue.";		
+			break;
+			
+		case 9:
+			animalS = "Snake";
+			message = "The Snake occupies the sixth position in the Chinese zodiac.\r\n" + 
+					"In Chinese culture, the Snake is the most enigmatic animal among the 12 zodiac animals. People born in a year of the Snake are supposed to be the most intuitive.";		
+			break;
+			
+		case 10:
+			animalS = "Horse";
+			message = "The Horse occupies the seventh position in the Chinese zodiac, after the Snake and before the Goat.\r\n" + 
+					"People born in a year of the Horse are extremely animated, active and energetic. Horses love to be in a crowd, and they can usually be seen on such occasions as concerts, theater performances, meetings, sporting events, and parties.";		
+			break;
+			
+		case 11:
+			animalS = "Goat";
+			message = "The Goat is eighth in the Chinese zodiac cycle.\r\n" + 
+					"People born in a year of the Goat are generally believed to be gentle, mild-mannered, shy, stable, sympathetic, amicable, and brimming with a strong sense of kindheartedness and justice.";		
+			break;
+			
+			
+		}
+		
+		
+		display(animalS, message);
+
 		
 		
 		scan.close();
 		
 	}
 	
+
+	
 	}
+
 	
 	
--- Test/src/Program.java
+++ Test/src/Program.java
@@ -1,13 +1,102 @@
 import java.util.Scanner;
-import java.lang.Math;
-public class Program {
-	
-	public static void main(String []args) {
-		
-		String x = "ABCDE";
-		
-		System.out.println(x.substring(0, 1));
 
+public class Program{  
+ 
+public static void main(String[] args) {  
+ 
+    
+ Scanner scan = new Scanner(System.in);
+ 
+ int i, sort = 0;
+ double sum = 0, MAX = 0, MIN = 0;
+ boolean one = true;
+ 
+ System.out.println("How long do you want the array?");
+ int length = scan.nextInt();
+ 
+ if(length > 0) {
+ 
+ double array[] = new double[length];
+ 
+ for(i = 0; i < length; i++) {
+  
+  System.out.println("Enter a number");
+  array[i] = scan.nextDouble();
+  sum += array[i];
+  if(one) {
+	  MAX = array[i];
+	  one = false;
+  }
+  if(array[i] > MAX)
+      MAX = array[i];
+  
+ }
+ 
+ MIN = MAX;
+ 
+ for(i = 0; i < length; i++) {
+  
+  if(array[i] < MIN)
+   MIN = array[i];
+  
+ }
+ 
+ System.out.print("Your array is {");
+ 
+    for(i = 0; i < length; i++) { 
+  
+  System.out.print(array[i]);
+  
+  if(i + 1 < length) {
+   
+   System.out.print(", ");
+   
+  }
+ }
+    
+    System.out.println("}");
+    
+    System.out.println(MAX);
+    
+    System.out.println("The average is " + sum/length);
+    System.out.println("The range is " + (MAX - MIN));
+    
+    sort = 1;
+    
+    for(i = 1; i < length; i++) {
+     
+     if(array[i - 1] > array[i])
+      sort = 0; 
+    }
+    
+    if(sort == 1)
+     System.out.println("The array is sorted in increasing order");
+    
+    else {
+    
+     sort = -1;
+     
+for(i = 1; i < length; i++) {
+     
+     if(array[i - 1] <= array[i])
+      sort = 0; 
+    }
 
+    if(sort == -1)
+     System.out.println("The array is sorted in decreasing order");
+
+    else {
+     System.out.println("The array is unsorted");
+    }
+    }
+ 
+ }else {
+  
+  System.out.println("Not a valid length!");
+  
+ }
+ 
+ scan.close();
+}  
 }
-}
+  
