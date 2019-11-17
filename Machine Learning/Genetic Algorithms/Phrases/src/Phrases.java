5592666974dce74ba59b39c6acf347b907f04efd
GioByte10 <gvanni.bernal10@gmail.com>
2019-11-17 14:20:24 -0800

    Java

--- Machine Learning/Genetic Algorithms/Phrases/src/Phrases.java
+++ Machine Learning/Genetic Algorithms/Phrases/src/Phrases.java
@@ -12,13 +12,17 @@ public class Phrases {
 		System.out.println("Machine Learning presents Genetic Algorithms:\n   Phrase by Natural Selection");
 		System.out.println("Enter a population:");
 		int maxPopulation = scan.nextInt();
+		
 		String[] populationArray = new String[maxPopulation];
 		int[] fitness = new int[maxPopulation];
+		
 		System.out.println("Enter a target (phrase):");
 		scan.nextLine();
 		String phrase = scan.nextLine();
+		
 		Population population = new Population(maxPopulation, mutationP, phrase);
 		DNA dna1 = new DNA(population, populationArray);
+		
 		dna1.generateDNA();
 		fitness = dna1.generateFitness();
 		
