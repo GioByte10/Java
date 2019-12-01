048578754d8904741a30158d830882c3d2a036a5
GioByte10 <gvanni.bernal10@gmail.com>
2019-11-30 18:53:59 -0800

    Java

--- Test/src/Program.java
+++ Test/src/Program.java
@@ -6,33 +6,34 @@ public class Program{
 	public static void main (String []args) {
 		
 boolean out = false;
+char x = 'i';
+
 		
-		String words = "Absorption, Achromatic, Adaptation, Aerobic, Algae, Alimentary, Allergy, Amoeba, Amphibian, Analogue, Anatomy, Ancestor, Antibody, Appendage, Aquatic, Arboreal, Asexual, Assimilation, Atrophy, Auditory" +
-	            "Backbone, Bacteria, Balance, Barrier, Benign, Biology, Biome, Bisect, Botany, Branch, Breathe" +
-	            "Carnivorous, Catalyst, Cavity, Cell, Chlorophyll, Cholesterol, Chromatic, Chromosome, Cilia, Classification, Cohesive, Collagen, Communicable, Community, Contraction, Control, Convolution, Coronary, Cyclic" +
-	            "Darwin, Deciduous, Decomposition, Dermis, Develop, Dichotomous, Dietary, Differentiation, Diffusion, Digestion, Dinosaur, Discipline, Displacement, Division, Dna, Dominant, Dormant" +
-	            "Ecology, Ecosystem, Ectoplasm, Edema, Embryo, Endangered, Endemic, Endoskeleton, Endothermic, Energy, Environment, Enzyme, Epidermis, Erosion, Essential, Estrogen, Evolution, Exoskeleton, Exothermic, Extermination, Extinct" +
-	            "Factor, Feedback, Fertilization, Fetus, Fibrillation, Filament, Fish, Fission, Flower, Food chain, Fossil, Function" +
-	            "Gamete, Gaseous, Gastrin, Genetics, Genome, Genus, Germ, Gestation, Gills, Grafting, Greenhouse effect, Growth" +
-	            "Habitat, Hemoglobin, Herbivore, Heredity, Heterogeneous, Hibernation, Hierarchy, Homogeneous, Hormone, Horticulture, Host, Human, Hybrid" +
-	            "Immunity, Inclusion, Incubate, Ingestion, Inherited traits, Inoculate, Inquiry, Insoluble, Instinct, Insulate, Insulin, Interdependent, Invertebrate, Involuntary, Isolate, Isotope" +
-	            "Juncture" +
-	            "Kinesthesia, Kingdom" +
-	            "Latitude, Leaf, Life cycle, Linkage, Living, Luminescent, Lymph" +
-	            "Malignant, Mammal, Marine, Meiosis, Membrane, Memory, Metabolism, Microbiology, Microscope, Migration, Mildew, Molt, Muscle, Mutation, Mutualism" +
-	            "Natural, Nerve, Neuron, Nitrogen, Nonliving, Nucleus" +
-	            "Omnivorous, Optic, Order, Organs, Osmosis, Ossification, Oviparous, Oxidation" +
-	            "Paleontologist, Parasite, Parasympathetic, Pathogen, Permeable, Photosynthesis, Plankton, Plasma, Platelet, Pollen, Pollution, Portion, Predator, Preserve, Pressure, Producer, Protein, Protoplasm, Pulmonary, Pyramid" +
-	            "Quest, Question" +
-	            "Radial, Receptor, Recessive, Recycling, Reflex, Regeneration, Regulate, Replicate, Repress, Reptile, Residual, Resource, Respiration, Restriction, RH factor, Root" +
-	            "Scope, Secrete, Seed, Sensor, Shelter, Skeleton, Skin, Solar, Spatial, Species, Specimen, Statistics, Stimulus, Stress, Structure, Symbiosis, Symmetry, Synapse, System" +
-	            "Taste, Taxonomy, Tendril, Terrestrial, Tissue, Tolerance, Toxin, Transformation, Transpiration, Tundra, Turgid" +
-	            "Unsaturated" +
-	            "Vacuole, Variation, Ventilate, Vertebrate, Viable, Virus, Vital, Vitamin, Viviparous, Volume, Voluntary" +
-	            "Waste, Weather" +
-	            "X chromosome" +
-	            "Y chromosome" +
-	            "Zone, Zoology";
+		String words = "Absolute Zero, Acceleration, Accuracy, Acoustics, Action, Adhesion, Affect, Alteration, Amplitude, Angular, Antiproton, Apparent, Applied, Apply, Aptitude, Aristotle, Aspect, Atmosphere, Atom, Atomic, Attenuate, Attraction, Aviation, Axis\r\n" + 
+				"Balance, Basic, Bodies, Boson, Branch\r\n" + 
+				"Calculus, Capacity, Centrifugal, Changes, Chaos, Charge, Chemical, Chromatics, Circular, Collide, Component, Compression, Condensation, Conductivity, Conservation, Constant, Control, Conversion, Copernicus, Cosmology, Curie\r\n" + 
+				"Damping, Decibel, Density, Dependence, Deposits, Design, Development, Deviation, Diode, Direction, Discipline, Displacement, Distance, Distortion, Disturbance, Doctorate, Drag, Dynamics\r\n" + 
+				"Effect, Einstein, Elasticity, Electricity, Electromagnetic, Electron, Electronics, Emission, Energy, Engine, Engineer, Entropy, Equation, Equilibrium, Equivalent, Evaporation, Expansion, Experiment, Explosion, External\r\n" + 
+				"Factor, Fission, Flight, Flow, Fluid, Flying, Focus, Force, Form, Formula, Frequency, Friction, Fulcrum, Fundamental\r\n" + 
+				"Galileo, Gas, Generate, Geophysics, Gilbert, Graph, Gravitation, Gravity, Guide, Gyroscope\r\n" + 
+				"Half-life, Harness, Heat, Hertz, Hold, Hydraulics\r\n" + 
+				"Identical, Impact, Impulse, Incandescent, Inertia, Influence, Innovate, Inquiry, Interaction, Intervening, Invention, Inverse, Investigation, Invisible, Irradiate, Isotope\r\n" + 
+				"Joule\r\n" + 
+				"Kaon, Kinetics\r\n" + 
+				"Laser, Laws, Level, Lift, Light, Liquids, Loft, Lumen\r\n" + 
+				"Machinery, Magnet, Magnetism, Magnitude, Mass, Mathematics, Matter, Measure, Mechanics, Meteorology, Metrics, Modulate, Molecule, Motion, Movement\r\n" + 
+				"Navigation, Negative, Newton, Nuclear, Nucleus\r\n" + 
+				"Objects, Optics, Oscillation, Overload\r\n" + 
+				"Parallax, Parity, Particle, Performance, Phenomenon, Photon, Piston, Pitch, Plasma, Position, Prediction, Pressure, Principal, Projectile, Propagation, Properties, Proportion, Psi, Pulse\r\n" + 
+				"Quality, Quandary, Quanta, Quantitative, Quantity, Quantum, Quark\r\n" + 
+				"Radiation, Radioactivity, Reception, Relationship, Relative, Relativity, Repulsion, Research, Resistance, Resolution, Resonance, Rest, Reversal, Robotic, Rotation\r\n" + 
+				"Saturation, Scalar, Science, Scientific, Seismology, Semiconductor, Shape, Shift, Newton, Solid, Sonic, Sound, Space, Spectrum, Speed, Standing, Stationary, Statistics, Strain, Stress, Structure, Study, Sublimation, Substance, Surface, Suspension, Symmetry, System\r\n" + 
+				"Temperature, Tensile, Tension, Theoretical, Theory, Thermal, Thermodynamics, Thrust, Torque, Training, Trajectory, Transformation, Transistor, Transition, Trough, Turning\r\n" + 
+				"Ultrasonic, Uncertainty, Uniform, Unify, Unique, Unit, Universal, University\r\n" + 
+				"Vacuum, Value, Vapor, Variable, Variation, Vector, Velocity, Viscosity, Volatile, Voltage, Volume\r\n" + 
+				"Wave, Weight, White noise\r\n" + 
+				"X-ray\r\n" + 
+				"Zone";
 
 		
 		String newS = "";
@@ -42,7 +43,7 @@ boolean out = false;
 		
 for(i = 0; i < words.length(); i++) {
 			
-			if(!(words.charAt(i) == ',' || words.charAt(i) == ' ')) {
+			if(!(words.charAt(i) == ',' || words.charAt(i) == ' ' || words.charAt(i) == '\n' || words.charAt(i) == '\r')) {
 				
 				newS += words.charAt(i);
 				
