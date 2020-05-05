2a1e8373b0307ef1f5416ced88ea1a9178e01915
GioByte10 <gvanni.bernal10@gmail.com>
2020-05-04 19:45:36 -0600

    Java

--- APCalendar/src/APCalendar.java
+++ APCalendar/src/APCalendar.java
@@ -20,4 +20,24 @@ public class APCalendar {
 		return count;
 	}
 	
+	public static int dayOfWeek(int month, int day, int year) {
+		
+		int dayW = firstDayOfYear(year) + dayOfYear(month, dayW, year);
+		dayW %= 7;
+		dayW--;
+		
+		return dayW;	
+		
+	}
+
+	private static int dayOfYear(int month, int day, int year) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int firstDayOfYear(int year) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
